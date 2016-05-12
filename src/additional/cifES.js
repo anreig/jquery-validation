38fbc896ac3daea4499c9e1f8ac964eaf320ff05
anreig <anreig@gmail.com>
2016-05-12 16:55:56 +0200

    Update cifES.js

--- src/additional/cifES.js
+++ src/additional/cifES.js
@@ -1,61 +1,48 @@
-/*
- * Código de identificación fiscal ( CIF ) is the tax identification code for Spanish legal entities
- * Further rules can be found in Spanish on http://es.wikipedia.org/wiki/C%C3%B3digo_de_identificaci%C3%B3n_fiscal
- */
-$.validator.addMethod( "cifES", function( value ) {
-	"use strict";
-
-	var num = [],
-		controlDigit, sum, i, count, tmp, secondDigit;
-
-	value = value.toUpperCase();
-
-	// Quick format test
-	if ( !value.match( "((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)" ) ) {
-		return false;
-	}
-
-	for ( i = 0; i < 9; i++ ) {
-		num[ i ] = parseInt( value.charAt( i ), 10 );
-	}
-
-	// Algorithm for checking CIF codes
-	sum = num[ 2 ] + num[ 4 ] + num[ 6 ];
-	for ( count = 1; count < 8; count += 2 ) {
-		tmp = ( 2 * num[ count ] ).toString();
-		secondDigit = tmp.charAt( 1 );
-
-		sum += parseInt( tmp.charAt( 0 ), 10 ) + ( secondDigit === "" ? 0 : parseInt( secondDigit, 10 ) );
-	}
-
-	/* The first (position 1) is a letter following the following criteria:
-	 *	A. Corporations
-	 *	B. LLCs
-	 *	C. General partnerships
-	 *	D. Companies limited partnerships
-	 *	E. Communities of goods
-	 *	F. Cooperative Societies
-	 *	G. Associations
-	 *	H. Communities of homeowners in horizontal property regime
-	 *	J. Civil Societies
-	 *	K. Old format
-	 *	L. Old format
-	 *	M. Old format
-	 *	N. Nonresident entities
-	 *	P. Local authorities
-	 *	Q. Autonomous bodies, state or not, and the like, and congregations and religious institutions
-	 *	R. Congregations and religious institutions (since 2008 ORDER EHA/451/2008)
-	 *	S. Organs of State Administration and regions
-	 *	V. Agrarian Transformation
-	 *	W. Permanent establishments of non-resident in Spain
-	 */
-	if ( /^[ABCDEFGHJNPQRSUVW]{1}/.test( value ) ) {
-		sum += "";
-		controlDigit = 10 - parseInt( sum.charAt( sum.length - 1 ), 10 );
-		value += controlDigit;
-		return ( num[ 8 ].toString() === String.fromCharCode( 64 + controlDigit ) || num[ 8 ].toString() === value.charAt( value.length - 1 ) );
-	}
-
-	return false;
-
-}, "Please specify a valid CIF number." );
+$.validator.addMethod( "cifES",function validaCif(texto)
+{ 
+  if (texto=="")
+  {
+	return;
+  }
+       
+        var pares = 0; 
+        var impares = 0; 
+        var suma; 
+        var ultima; 
+        var unumero; 
+        var uletra = new Array("J", "A", "B", "C", "D", "E", "F", "G", "H", "I"); 
+        var xxx; 
+         
+        texto = texto.toUpperCase(); 
+         
+        var regular = new RegExp(/^[ABCDEFGHKLMNPQS]\d\d\d\d\d\d\d[0-9,A-J]$/g); 
+         if (!regular.exec(texto)) 
+			{
+				return false;
+				
+			}
+		 
+              
+         ultima = texto.substr(8,1); 
+ 
+         for (var cont = 1 ; cont < 7 ; cont ++){ 
+             xxx = (2 * parseInt(texto.substr(cont++,1))).toString() + "0"; 
+             impares += parseInt(xxx.substr(0,1)) + parseInt(xxx.substr(1,1)); 
+             pares += parseInt(texto.substr(cont,1)); 
+         } 
+         xxx = (2 * parseInt(texto.substr(cont,1))).toString() + "0"; 
+         impares += parseInt(xxx.substr(0,1)) + parseInt(xxx.substr(1,1)); 
+          
+         suma = (pares + impares).toString(); 
+         unumero = parseInt(suma.substr(suma.length - 1, 1)); 
+         unumero = (10 - unumero).toString(); 
+         if(unumero == 10) unumero = 0; 
+          
+         if ((ultima == unumero) || (ultima == uletra[unumero])) 
+             return true; 
+         else 
+             {
+				return false;
+			 }
+ 
+    } , "Please specify a valid CIF number." );
